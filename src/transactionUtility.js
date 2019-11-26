be4876fa88d67ef9624c02f52aee978836fea01d
= <=>
2019-11-26 19:33:25 +0530

    query with date and employeeId feature added
    modified query function for query by empId and date both
    add a mapper and predicate for filtering data by empId and date
    
    changes need to be made to make query work
    with empId and date

--- src/transactionUtility.js
+++ src/transactionUtility.js
@@ -1,32 +1,65 @@
 const fs = require("fs");
 const utils = require("./utils");
 
-const saveBeverageEntry = function(empBeverageRecords, beverageEntry, time) {
-  const empId = beverageEntry["empId"];
-  const beverage = beverageEntry["beverage"];
-  const quantity = +beverageEntry["qty"];
-  if (!empBeverageRecords.hasOwnProperty(empId)) {
-    empBeverageRecords[empId] = {
-      empId: +empId,
-      orders: [],
-      beverageCount: 0
-    };
-  }
-  empBeverageRecords[empId].orders.push({ beverage, quantity, time });
-  empBeverageRecords[empId].beverageCount += quantity;
-
-  return { empBeverageRecords, beverageEntry };
+const inBeverageOrderAdd = function(empId) {
+	return function(beverageRecord) {
+		beverageRecord["empId"] = empId;
+		return beverageRecord;
+	};
 };
 
-const queryBeverageEntry = function(empBeverageRecords, beverageEntry) {
-  const empId = beverageEntry["empId"];
-  let beveragesConsumed = [];
-  let noOfBeverageConsumed = 0;
-  if (empBeverageRecords.hasOwnProperty(empId)) {
-    noOfBeverageConsumed = empBeverageRecords[empId].beverageCount;
-    beveragesConsumed = empBeverageRecords[empId].orders;
-  }
-  return { empId, beveragesConsumed, noOfBeverageConsumed };
+const getBeverageRecordList = function(beverageRecords) {
+	let beverageRecordList = [];
+	for (let empBeverageRecord in beverageRecords) {
+		const empbeverageOrders = beverageRecords[empBeverageRecord]["orders"];
+		const empId = empBeverageRecord;
+		const beverageOrders = empbeverageOrders.map(inBeverageOrderAdd(empId));
+		beverageRecordList = beverageRecordList.concat(beverageOrders);
+	}
+	return beverageRecordList;
+};
+
+const saveBeverageEntry = function(empBeverageRecords, beverageEntry, time) {
+	const empId = beverageEntry["empId"];
+	const beverage = beverageEntry["beverage"];
+	const quantity = +beverageEntry["qty"];
+	if (!empBeverageRecords.hasOwnProperty(empId)) {
+		empBeverageRecords[empId] = {
+			empId: +empId,
+			orders: [],
+			beverageCount: 0,
+		};
+	}
+	empBeverageRecords[empId].orders.push({ beverage, quantity, time });
+	empBeverageRecords[empId].beverageCount += quantity;
+
+	return { empBeverageRecords, beverageEntry };
+};
+
+const recordsHaving = function(employeeId, time) {
+	return function(beverageRecord) {
+		const empId = employeeId || beverageRecord["empId"];
+		date = time || beverageRecord["time"];
+		const desiredDate = new Date(date).toLocaleDateString();
+		const actualDate = new Date(beverageRecord["time"]).toLocaleDateString();
+		const validEmpId = empId === beverageRecord.empId;
+		const validDate = desiredDate === actualDate;
+		return validEmpId && validDate;
+	};
+};
+
+const queryBeverageEntry = function(empBeverageRecords, queryData) {
+	const beverageRecordList = getBeverageRecordList(empBeverageRecords);
+	const { empId, date } = queryData;
+	const selectedRecords = beverageRecordList.filter(recordsHaving(empId, date));
+	const noOfBeverageConsumed = selectedRecords.reduce(function(
+		totalQuantity,
+		record
+	) {
+		return totalQuantity + record["quantity"];
+	},
+	0);
+	return { selectedRecords, noOfBeverageConsumed };
 };
 
 exports.save = saveBeverageEntry;
--- test/testTransactionUtility.js
+++ test/testTransactionUtility.js
@@ -3,95 +3,95 @@ const assert = require("assert");
 const fs = require("fs");
 
 describe("update transactionRecord with latest entry given", function() {
-  const time = new Date("2019-11-25T19:25:19.474Z");
-  it("should update empty object with given entry", function() {
-    const beverageEntry = {
-      empId: "1111",
-      beverage: "orange",
-      qty: "1"
-    };
-    const expected = {
-      empBeverageRecords: {
-        "1111": {
-          empId: 1111,
-          orders: [{ beverage: "orange", quantity: 1, time: time }],
-          beverageCount: 1
-        }
-      },
-      beverageEntry: {
-        empId: "1111",
-        beverage: "orange",
-        qty: "1"
-      }
-    };
-    const actual = transactionUtility.save({}, beverageEntry, time);
-    assert.deepStrictEqual(actual, expected);
-  });
+	const time = new Date("2019-11-25T19:25:19.474Z");
+	it("should update empty object with given entry", function() {
+		const beverageEntry = {
+			empId: "1111",
+			beverage: "orange",
+			qty: "1",
+		};
+		const expected = {
+			empBeverageRecords: {
+				"1111": {
+					empId: 1111,
+					orders: [{ beverage: "orange", quantity: 1, time: time }],
+					beverageCount: 1,
+				},
+			},
+			beverageEntry: {
+				empId: "1111",
+				beverage: "orange",
+				qty: "1",
+			},
+		};
+		const actual = transactionUtility.save({}, beverageEntry, time);
+		assert.deepStrictEqual(actual, expected);
+	});
 
-  it("should update record with given entry if employee already exists", function() {
-    const beverageEntry = {
-      empId: "1111",
-      beverage: "banana",
-      qty: "1"
-    };
-    const lastObject = {
-      "1111": {
-        empId: 1111,
-        orders: [{ beverage: "orange", quantity: 1, time: time }],
-        beverageCount: 1
-      }
-    };
-    const expected = {
-      empBeverageRecords: {
-        "1111": {
-          empId: 1111,
-          orders: [
-            { beverage: "orange", quantity: 1, time: time },
-            { beverage: "banana", quantity: 1, time: time }
-          ],
-          beverageCount: 2
-        }
-      },
-      beverageEntry: {
-        empId: "1111",
-        beverage: "banana",
-        qty: "1"
-      }
-    };
-    const actual = transactionUtility.save(lastObject, beverageEntry, time);
-    assert.deepStrictEqual(actual, expected);
-  });
+	it("should update record with given entry if employee already exists", function() {
+		const beverageEntry = {
+			empId: "1111",
+			beverage: "banana",
+			qty: "1",
+		};
+		const lastObject = {
+			"1111": {
+				empId: 1111,
+				orders: [{ beverage: "orange", quantity: 1, time: time }],
+				beverageCount: 1,
+			},
+		};
+		const expected = {
+			empBeverageRecords: {
+				"1111": {
+					empId: 1111,
+					orders: [
+						{ beverage: "orange", quantity: 1, time: time },
+						{ beverage: "banana", quantity: 1, time: time },
+					],
+					beverageCount: 2,
+				},
+			},
+			beverageEntry: {
+				empId: "1111",
+				beverage: "banana",
+				qty: "1",
+			},
+		};
+		const actual = transactionUtility.save(lastObject, beverageEntry, time);
+		assert.deepStrictEqual(actual, expected);
+	});
 });
 
 describe("queryBeverageEntry", function() {
-  const time = new Date("2019-11-25T19:25:19.474Z");
-  it("should give employee beverageRecord if employee exists", function() {
-    const empBeverageRecords = {
-      "1111": {
-        empId: 1111,
-        orders: [{ beverage: "orange", quantity: 1, time: time }],
-        beverageCount: 1
-      }
-    };
-    const beverageEntry = { empId: "1111" };
-    const actual = transactionUtility.query(empBeverageRecords, beverageEntry);
-    const expected = {
-      empId: "1111",
-      beveragesConsumed: [{ beverage: "orange", quantity: 1, time: time }],
-      noOfBeverageConsumed: 1
-    };
-    assert.deepStrictEqual(actual, expected);
-  });
+	const time = new Date("2019-11-25T19:25:19.474Z");
+	it("should give employee beverageRecord if employee exists", function() {
+		const empBeverageRecords = {
+			"1111": {
+				empId: 1111,
+				orders: [{ beverage: "orange", quantity: 1, time: time }],
+				beverageCount: 1,
+			},
+		};
+		const beverageEntry = { empId: "1111" };
+		const actual = transactionUtility.query(empBeverageRecords, beverageEntry);
+		const expected = {
+			selectedRecords: [
+				{ empId: "1111", beverage: "orange", quantity: 1, time: time },
+			],
+			noOfBeverageConsumed: 1,
+		};
+		assert.deepStrictEqual(actual, expected);
+	});
 
-  it("should give employee beverageRecord if employee does not exists", function() {
-    const empBeverageRecords = {};
-    const beverageEntry = { empId: "1111" };
-    const actual = transactionUtility.query(empBeverageRecords, beverageEntry);
-    const expected = {
-      empId: "1111",
-      beveragesConsumed: [],
-      noOfBeverageConsumed: 0
-    };
-    assert.deepStrictEqual(actual, expected);
-  });
+	it("should give employee beverageRecord if employee does not exists", function() {
+		const empBeverageRecords = {};
+		const beverageEntry = { empId: "1111" };
+		const actual = transactionUtility.query(empBeverageRecords, beverageEntry);
+		const expected = {
+			selectedRecords: [],
+			noOfBeverageConsumed: 0,
+		};
+		assert.deepStrictEqual(actual, expected);
+	});
 });
